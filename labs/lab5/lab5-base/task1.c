d0ef817327ebf0c6c7b4b15c0695a4152d4345ad
Brendan Klayman <bklayma1@cs.binghamton.edu>
2020-10-06 21:09:33 -0400

    Why does lab 5 not work

--- labs/lab5/lab5-base/task1.c
+++ labs/lab5/lab5-base/task1.c
@@ -8,12 +8,43 @@
 
 int main(void) {
     char * argv1[] = {"cat", "Makefile", 0};
-    char * argv2[] = {"head", "-4", 0};
-    //char * argv2[] = {"wc", "-l", 0};
+    //char * argv2[] = {"head", "-4", 0};
+    char * argv2[] = {"wc", "-l", 0};
     
     setbuf(stdout, NULL);
 
+     
+    int processID = fork();
+    int fd[2];
+    pipe(fd);
 
+    if(processID == 0){
+	printf("In CHILD-1 (PID=%i): executing command %s ...\n", getpid(), argv1[0]);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	close(fd[1]);
+	int execReturn = execvp(argv1[0], argv1);
+	fprintf(stderr, "Child 1 Failed\n");
+	exit(1);
+    }
+    processID = fork();
+    if(processID > 0){
+	close(fd[0]);
+	close(fd[1]);
+	int reapID = wait(NULL);
+	while(reapID > 0){
+		printf("In PARENT (PID=%i): successfully reaped child (PID=%i)\n", getpid(), reapID);
+		reapID = wait(NULL);
+	}
+    } else {
+	printf("In Child-2 (PID=%i): executing command %s ...\n", getpid(), argv2[0]);
+	dup2(fd[0], 0);
+	close(fd[1]);
+	close(fd[0]);
+	execvp(argv2[0], argv2);
+	fprintf(stderr, "Child 2 failed\n");
+	exit(1);
+    }
 
     return 0;
 }
